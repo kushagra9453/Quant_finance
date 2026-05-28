6f57abe3853a99a14ae0711be9f4951b6d9faf0e
Krishna <krishna@Krishnas-MacBook.local>
2026-05-28 16:08:05 +0530

    updated project files

--- python_advance.py/if,else.py
+++ python_advance.py/if,else.py
@@ -1,25 +1,21 @@
-from operators import profit_percentage
 buy_price = 500
 current_price = 620
 
-# Calculate profit percentage
-# Then classify:
-# - profit % > 20 → "Excellent trade"
-# - profit % > 10 → "Good trade"
-# - profit % > 0  → "Small profit"
-# - profit % == 0 → "Breakeven"
-# - profit % < 0  → "In loss"
+profit_percentage = ((current_price - buy_price) * 100) / buy_price
 
-# # Print the profit % and classification
-print(f"profit_percentage:{(current_price-buy_price/buy_price*100)}")
-if profit_percentage>20:
-    print("excellent")
-elif profit_percentage>10:
-    print("good")
-elif profit_percentage>0:
-    print("small profit")
-elif profit_percentage==0:
-    print("breakeven")
+print(f"profit_percentage: {profit_percentage}")
+
+if profit_percentage > 20:
+    print("Excellent trade")
+
+elif profit_percentage > 10:
+    print("Good trade")
+
+elif profit_percentage > 0:
+    print("Small profit")
+
+elif profit_percentage == 0:
+    print("Breakeven")
+
 else:
-    print("loss")
-    
+    print("Loss")
